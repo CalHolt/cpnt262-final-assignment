9851e687edcf65bf761ad2b97ccd0f36eefb5274
drib <calumholt@hotmail.com>
2023-11-15 11:15:07 -0700

    hero section added

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="bg-black flex justify-between p-2">
-  <div class="text-white"> <a href="/">Logo</a></div>
+  <div class="text-white"> <a href="/">SkatePaint</a></div>
   <nav>
     <ul class="flex justify-around gap-4">
       <li class="text-white">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+<header class="bg-slate-700 ">
+  <h1 class="text-3xl text-white">Design a deck today</h1>
+  <p class="text-lg text-white">We now offer the option to create a custom board online! Try making your own today ...</p>
+  <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+    Customize Now
+    </span>
+    </button>
+</header>
 
+<style>
+  header {
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items:center;
 
+    text-align: center;
+  }
+</style>
