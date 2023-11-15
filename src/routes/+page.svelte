acc9dbf51ffe3457d7c45742041aad5052f9e739
drib <calumholt@hotmail.com>
2023-11-15 09:02:22 -0700

    test card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>  
 import Card from "../lib/components/Card.svelte"
+import { galleryInformation } from "../lib/data"
 </script>
-<Card />
+<Card {...galleryInformation}/>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
