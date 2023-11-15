b16fd50e565f16039ffec6557b76cc2bb508da5c
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-15 10:40:26 -0700

    changes to the svelte head

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 import Card from "../../lib/components/Card.svelte";
 import { galleryInformation } from "../../lib/data";
+let title = "gallery page"
 </script>
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 <main class = "p-6 grid grid-cols-1 gap-4 justify-items-center align-items-center md:grid-cols-2 lg:grid-cols-4">
   {#each galleryInformation as info}
   <Card {...info}/>
