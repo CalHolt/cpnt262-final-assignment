6c3cfbd56891eb15e07daa2ed38c4860308e8867
drib <calumholt@hotmail.com>
2023-11-15 08:50:54 -0700

    card component fixes

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,9 +1,18 @@
-<header>
-  <h1>
-    Cpnt-262-Final Project
-  </h1>
-</header>
-
-<Footer>
-  Copyright 2023 | Cal and Kevin Project
-</Footer>
+<script>
+  export let cardTitle;
+  export let cardDescription;
+  export let cardImg; 
+  export let cardLink;
+  export let altText;
+</script>
+<figure class="max-w-xs bg-rose-900 hover:bg-rose-950 rounded-3xl p-6">
+  <a href="{cardLink}">
+  <img class="rounded-2xl" src="{cardImg}" alt="{altText}">
+  
+  <h3 class="text-white py-2"><strong>{cardTitle}</strong></h3>
+ 
+  <p class="text-white py-2">
+    <em>{cardDescription}</em>
+  </p>
+</a>
+</figure> 
