14c45c8bc400086f1e0f865962e379cd12469a49
drib <calumholt@hotmail.com>
2023-11-15 10:31:53 -0700

    added gallery page and routed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,2 @@
-<script>  
-import Card from "../lib/components/Card.svelte"
-import { galleryInformation } from "../lib/data"
-</script>
-<main class = "p-6 grid grid-cols-1 gap-4 justify-items-center align-items-center md:grid-cols-2 lg:grid-cols-4">
-  {#each galleryInformation as info}
-  <Card {...info}/>
-  {/each}
-</main>
+
 
