dd9f83df278850de1090505e7eff65d220a0921f
drib <calumholt@hotmail.com>
2023-11-15 09:28:37 -0700

    styled gallery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 import Card from "../lib/components/Card.svelte"
 import { galleryInformation } from "../lib/data"
 </script>
-{#each galleryInformation as info}
-<Card {...info}/>
-{/each}
+<main class = "p-6 grid grid-cols-1 gap-4 justify-items-center align-items-center md:grid-cols-2 lg:grid-cols-4">
+  {#each galleryInformation as info}
+  <Card {...info}/>
+  {/each}
+</main>
 
