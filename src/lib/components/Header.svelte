fe2ee9cd9157146713880ecb6bc8ab43cd253237
drib <calumholt@hotmail.com>
2023-11-15 10:19:54 -0700

    header added

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<header class="bg-black flex justify-between p-2">
+  <div class="text-white"> <a href="/">Logo</a></div>
+  <nav>
+    <ul class="flex justify-around gap-4">
+      <li class="text-white">
+          <a href="/gallery">Gallery</a>
+      </li>
+      <li class="text-white">
+        <a href="/store">Store</a>
+      </li>
+      <li class="text-white">
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
