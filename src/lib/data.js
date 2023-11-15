079e89f55b666522dbe5f3964041d5108576ba1a
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-15 09:26:34 -0700

    changes to alttext attributes

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,27 +1,33 @@
 export let galleryInformation = [{
   cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/camera.jpg"},
+  cardImg: "../images/camera.jpg", 
+ altText: "old camera on piano"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/autumn-leaves.jpg"},
+  cardImg: "../images/autumn-leaves.jpg",
+  altText: "yellow and orange leaves on the ground"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/desert-starry-night.jpg"},
+  cardImg: "../images/desert-starry-night.jpg", 
+ ltText: "desert at dusk"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/laptop-with-mouse.jpg"},
+  cardImg: "../images/laptop-with-mouse.jpg",
+ altText: "laptop on table"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/mountains.jpg"},
+  cardImg: "../images/mountains.jpg",
+ altText: "mountaintop at midday"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
-  cardImg: "../images/ocean-shore.jpg"},
+  cardImg: "../images/ocean-shore.jpg",
+altText: "rock being hit by waves on beach"},
   {
     cardTitle: "hello world",
   cardDes: "whats going on dude",
