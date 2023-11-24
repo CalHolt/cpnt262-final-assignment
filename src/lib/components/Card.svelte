21a1e7288c95120bd7d98918eb6ecd170e7517aa
drib <calumholt@hotmail.com>
2023-11-23 18:52:51 -0700

    added styles

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -5,14 +5,14 @@
   export let slug;
   export let altText;
 </script>
-<figure class="max-w-xs bg-rose-900 hover:bg-rose-950 rounded-3xl p-6">
+<figure class="max-w-xs bg-slate-600 hover:bg-rose-950 rounded-3xl p-6">
   <a href="/gallery/{slug}">
   <img class="rounded-2xl" src="{cardImg}" alt="{altText}">
   
   <h3 class="text-white py-2"><strong>{cardTitle}</strong></h3>
  
   <p class="text-white py-2">
-    <em>{cardDes}</em>
+    <em class="text-white">{cardDes}</em>
   </p>
 </a>
 </figure>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,9 @@
 <header class="bg-black flex justify-between p-2">
-  <div class="text-white"> <a href="/">SkatePaint</a></div>
+  <div class="text-white mx-2"> <a href="/">SkatePaint</a></div>
   <nav>
     <ul class="flex justify-around gap-4">
       <li class="text-white">
-          <a href="/gallery">Gallery</a>
+          <a class="m-2" href="/gallery">Gallery</a>
       </li>
     </ul>
   </nav>
