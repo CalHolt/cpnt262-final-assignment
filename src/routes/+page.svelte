1eaf5b0771b9713e13f7d4af206677231a9e43d5
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-24 14:26:12 -0700

    alert button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 <body>
 <header class="bg-cover bg-center ..." style="background-image: url(https://images.unsplash.com/photo-1547447134-cd3f5c716030?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
   <h1 class="text-5xl text-white">Design a deck today</h1>
+  <button onclick="document.getElementById('demo').innerHTML=Date()">The time is?</button><button onclick="document.getElementById('demo').innerHTML=Date()">Create your Board</button>
   <p class="text-lg text-white">We now offer the option to create a custom board online! Try making your own today ...</p>
   <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
