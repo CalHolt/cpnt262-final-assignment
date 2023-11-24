0ab47cb5a7103828e869c15e169986e6dfe75ac4
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-24 14:33:07 -0700

    event handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,15 @@
   import ReviewCard from "../lib/components/ReviewCard.svelte";
   import { reviewInfo } from "../lib/data";
 </script>
+<div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+  <div class="flex">
+    <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+    <div>
+      <p class="font-bold">Welcome to Cal and Kev's website</p>
+      <p class="text-sm">Make sure you know how these changes affect you.</p>
+    </div>
+  </div>
+</div>
 <body>
 <header class="bg-cover bg-center ..." style="background-image: url(https://images.unsplash.com/photo-1547447134-cd3f5c716030?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
   <h1 class="text-5xl text-white">Design a deck today</h1>
